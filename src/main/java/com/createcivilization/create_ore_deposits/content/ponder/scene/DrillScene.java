c2ee3c6928f6ac8ff567b787ef97bb75d501d5c7
Arctic <creepergaming490@gmail.com>
2025-04-24 01:12:45 -0400

    More ponder

--- src/main/java/com/createcivilization/create_ore_deposits/content/ponder/scene/DrillScene.java
+++ src/main/java/com/createcivilization/create_ore_deposits/content/ponder/scene/DrillScene.java
@@ -8,81 +8,102 @@ import net.createmod.ponder.api.scene.SceneBuildingUtil;
 import net.createmod.ponder.api.scene.Selection;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.function.UnaryOperator;
 
 public class DrillScene {
-    public static void basic(SceneBuilder builder, SceneBuildingUtil util){
 
-        //I had a feeling you would look here, what if when the drill mines and drops unrefined ore
-        //We make it so they have to like test the unrefined ore to see what mineral is in the deposit'
-        //Idk tho
+    public static void basic(SceneBuilder builder, SceneBuildingUtil util) {
 
         CreateSceneBuilder scene = new CreateSceneBuilder(builder);
-        scene.title("drill.intro", "Setting Up And Using A Drill");
-        scene.configureBasePlate(0,0, 5);
+        scene.title("drill.intro", "Setting Up and Using a Drill");
+        scene.configureBasePlate(0, 0, 5);
         scene.showBasePlate();
-
         scene.idle(5);
 
-        Selection drill = util.select().position(2,5,2);
-        Selection cogAndShaft = util.select().fromTo(0,2,5,1,5,2);
-        BlockPos depositPos = util.grid().at(2,0,2);
-        Selection funnel = util.select().position(3,5,2);
+        // === Selections ===
+        Selection drill = util.select().position(2, 5, 2);
+        Selection cogAndShaft = util.select().fromTo(0, 2, 5, 1, 5, 2);
+        Selection funnel = util.select().position(3, 5, 2);
+        Selection depositVein = util.select().fromTo(0, 1, 0, 3, 2, 3);
+        Selection kinetics = util.select().fromTo(0, 2, 5, 2, 5, 2);
 
+        BlockPos depositPos = util.grid().at(0, 1, 0);
+        BlockPos funnelPos = util.grid().at(3, 5, 2);
 
+        // === Show Drill ===
         ElementLink<WorldSectionElement> drillLink = scene.world().showIndependentSection(drill, Direction.UP);
-
-
         scene.idle(5);
+
         scene.overlay().showText(60)
-                .text("This is the drill... It Drills")
+                .text("This is the drill... it drills.")
                 .attachKeyFrame()
                 .placeNearTarget()
                 .pointAt(drill.getCenter());
         scene.idle(70);
+
+        // === Show Deposit Vein ===
+        ElementLink<WorldSectionElement> depositLink = scene.world().showIndependentSection(depositVein, Direction.WEST);
         scene.overlay().showText(60)
-                .text("This is the deposit. This is what the Drill Drills.")
+                .text("This is a deposit vein. This is what the drill mines.")
                 .attachKeyFrame()
                 .placeNearTarget()
-                .pointAt(util.vector().topOf(depositPos));
+                .pointAt(util.vector().centerOf(2, 1, 2));
         scene.idle(70);
+
+        // === Power the Drill ===
         scene.overlay().showText(60)
-                .text("Apply power to the drill")
+                .text("Apply power to the drill.")
                 .attachKeyFrame()
                 .placeNearTarget()
                 .pointAt(drill.getCenter());
         scene.idle(10);
+
         scene.world().showSectionAndMerge(cogAndShaft, Direction.EAST, drillLink);
         scene.idle(5);
-        Selection kinetics = util.select().fromTo(0,2,5,2,5,2);
-        scene.world().setKineticSpeed(kinetics, 32);
-        scene.idle(55);
 
+        scene.world().setKineticSpeed(kinetics, 32);
+        scene.idle(35);
+
+        scene.overlay().showText(40)
+                .attachKeyFrame()
+                .placeNearTarget()
+                .pointAt(util.vector().topOf(depositPos))
+                .text("When a running drill hits a deposit, it will break the furthest deposit in the vein.");
 
         for (int i = 0; i < 10; i++) {
-            scene.idle(10);
+            scene.idle(8);
             scene.world().incrementBlockBreakingProgress(depositPos);
-            if (i == 1) {
-                scene.overlay().showText(80)
+
+            if (i == 6) {
+                scene.overlay().showText(40)
                         .attachKeyFrame()
                         .placeNearTarget()
                         .pointAt(util.vector().topOf(depositPos))
-                        .text("When a running drill hits a deposit it will break it.");
+                        .text("It will keep doing this until the entire deposit vein is depleted.");
             }
         }
-        scene.idle(30);
-        scene.world().setKineticSpeed(kinetics, 0);
-        scene.idle(5);
+
+        scene.idle(40);
+        scene.world().hideIndependentSection(depositLink, Direction.WEST);
+        scene.idle(45);
         scene.world().showSection(funnel, Direction.WEST);
-        scene.idle(10);
-        scene.overlay().showText(60)
+        scene.idle(20);
+
+        scene.overlay().showText(80)
                 .attachKeyFrame()
                 .placeNearTarget()
                 .pointAt(funnel.getCenter())
                 .text("A max of 64 items are stored in the drill. Get them out.");
-        scene.idle(10);
+        scene.idle(40);
 
+        ItemStack rawIron = new ItemStack(Items.RAW_IRON, 64);
+        scene.world().flapFunnel(funnelPos, true);
+        scene.world().createItemEntity(funnelPos.getCenter(), Vec3.ZERO, rawIron);
+        scene.idle(10);
     }
 }
