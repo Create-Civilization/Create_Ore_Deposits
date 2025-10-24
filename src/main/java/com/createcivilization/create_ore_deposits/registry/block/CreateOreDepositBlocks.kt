fdd3cba68f62609a9c72190a9772da6c38a7da70
EpicVon2468 <thomaschad0909@gmail.com>
2025-10-25 10:55:19 +1100

    Example deposits begin

--- src/main/java/com/createcivilization/create_ore_deposits/registry/block/CreateOreDepositBlocks.kt
+++ src/main/java/com/createcivilization/create_ore_deposits/registry/block/CreateOreDepositBlocks.kt
@@ -1,8 +1,11 @@
 package com.createcivilization.create_ore_deposits.registry.block
 
 import com.createcivilization.create_ore_deposits.CreateOreDeposits
+import com.createcivilization.create_ore_deposits.util.Block
+import com.createcivilization.create_ore_deposits.util.BlockProvider
 import com.createcivilization.create_ore_deposits.util.KotlinDeferredRegister
 import com.createcivilization.create_ore_deposits.util.Registries
+import com.createcivilization.create_ore_deposits.util.getValue
 
 import net.minecraft.world.level.block.Block
 
@@ -14,6 +17,9 @@ object CreateOreDepositBlocks {
 		Registries.BLOCK,
 		CreateOreDeposits.MOD_ID
 	)
+
+	private val _EXAMPLE_DEPOSIT: BlockProvider = BLOCK_PROVIDER.register("example_deposit", ::Block)
+	val EXAMPLE_DEPOSIT: Block by _EXAMPLE_DEPOSIT
 
 	init { BLOCK_PROVIDER.register(MOD_BUS) }
 }
