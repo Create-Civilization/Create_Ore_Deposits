1dbd9d7602a57ba34cd54b75ba510ae037546cfb
Arctic <creepergaming490@gmail.com>
2025-10-27 15:48:03 -0400

    Broken.

--- src/main/java/com/createcivilization/create_ore_deposits/registry/fluid/CreateOreDepositsFluids.kt
+++ src/main/java/com/createcivilization/create_ore_deposits/registry/fluid/CreateOreDepositsFluids.kt
@@ -2,6 +2,7 @@ package com.createcivilization.create_ore_deposits.registry.fluid
 
 import com.createcivilization.create_ore_deposits.CreateOreDeposits
 import com.createcivilization.create_ore_deposits.CreateOreDeposits.REGISTRATE
+import com.createcivilization.create_ore_deposits.registry.fluid.entries.SlagFluidBlock
 import com.tterrag.registrate.util.entry.FluidEntry
 import net.neoforged.neoforge.fluids.BaseFlowingFluid
 
@@ -24,8 +25,9 @@ object CreateOreDepositsFluids {
 		}.register()
 
 
-	val SLAG: FluidEntry<BaseFlowingFluid.Flowing?>? = REGISTRATE.fluid("slag", CreateOreDeposits.rl("block/fluid/slag_still"), CreateOreDeposits.rl("block/fluid/slag_flow"))
-		.properties {b -> b.viscosity(1500).density(500)}.
-		fluidProperties {p -> p.levelDecreasePerBlock(2).tickRate(25).slopeFindDistance(25).explosionResistance(0f)}
-	.register()
+	val SLAG: FluidEntry<BaseFlowingFluid.Flowing?>? =
+		REGISTRATE.fluid("slag", CreateOreDeposits.rl("block/fluid/slag_still"), CreateOreDeposits.rl("block/fluid/slag_flow"))
+			.properties{b -> b.viscosity(1500).density(500)}.fluidProperties{p -> p.levelDecreasePerBlock(2).tickRate(25).slopeFindDistance(25).explosionResistance(0f)}
+			.block{p, t -> SlagFluidBlock(p, t)}
+			.register() as FluidEntry<BaseFlowingFluid.Flowing?>?
 }
---
+++ src/main/java/com/createcivilization/create_ore_deposits/registry/fluid/entries/SlagFluidBlock.kt
@@ -0,0 +1,25 @@
+package com.createcivilization.create_ore_deposits.registry.fluid.entries
+
+import net.minecraft.core.BlockPos
+import net.minecraft.world.damagesource.DamageSource
+import net.minecraft.world.damagesource.DamageSources
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.LiquidBlock
+import net.minecraft.world.level.block.state.BlockBehaviour
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.material.FlowingFluid
+import java.util.Properties
+import java.util.function.Supplier
+
+// Define your block class first:
+class SlagFluidBlock(fluid: FlowingFluid, properties: BlockBehaviour.Properties) : LiquidBlock(fluid, properties) {
+	override fun entityInside(state: BlockState, level: Level, pos: BlockPos, entity: Entity) {
+		if (entity is LivingEntity) {
+			entity.hurt(level.damageSources().inFire(), 4.0f);
+			entity.igniteForSeconds(3f);
+		}
+		super.entityInside(state, level, pos, entity)
+	}
+}
