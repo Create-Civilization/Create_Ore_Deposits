c032e635cb14fb2e939591383ebc9c5486afefb9
McArctic <creepergaming490@gmail.com>
2025-10-27 17:57:29 -0400

    Added molten_iron and slag textures. And added recipe for making molten_iron

--- src/main/java/com/createcivilization/create_ore_deposits/registry/fluid/CreateOreDepositsFluids.kt
+++ src/main/java/com/createcivilization/create_ore_deposits/registry/fluid/CreateOreDepositsFluids.kt
@@ -2,19 +2,34 @@ package com.createcivilization.create_ore_deposits.registry.fluid
 
 import com.createcivilization.create_ore_deposits.CreateOreDeposits
 import com.createcivilization.create_ore_deposits.CreateOreDeposits.REGISTRATE
+import com.createcivilization.create_ore_deposits.registry.fluid.entries.MoltenIronFluidBlock
 import com.createcivilization.create_ore_deposits.registry.fluid.entries.SlagFluidBlock
 import com.tterrag.registrate.util.entry.FluidEntry
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.state.BlockBehaviour
 import net.neoforged.neoforge.fluids.BaseFlowingFluid
 
 
 
-/**
- * DO NOT USE NEO'S HEAVILY ABSTRACTED FLUID CLASS! IT WORKS VERY DIFFERENTLY! TOO MUCH OF A HASSLE!
- */
+
 object CreateOreDepositsFluids {
 
+	fun fluidRL(name: String, flowing: Boolean): ResourceLocation = CreateOreDeposits.rl("block/fluid/$name/${name}_${if (flowing) "flow" else "still"}");
 
-	val LUBRICANT: FluidEntry<BaseFlowingFluid.Flowing?>? = REGISTRATE.fluid("lubricant",CreateOreDeposits.rl("block/fluid/lubricant_still"),CreateOreDeposits.rl("block/fluid/lubricant_flow"))
+	//Lubricant Textures
+	val LUBRICANT_STILL: ResourceLocation get() = fluidRL("lubricant", false);
+	val LUBRICANT_FLOWING: ResourceLocation get() = fluidRL("lubricant", true);
+
+	//Slag Textures
+	val SLAG_STILL: ResourceLocation get() = fluidRL("slag", false);
+	val SLAG_FLOWING: ResourceLocation get() = fluidRL("slag", true);
+
+	//Molten Iron
+	val MOLTEN_IRON_STILL: ResourceLocation get() = fluidRL("molten_iron", false);
+	val MOLTEN_IRON_FLOWING: ResourceLocation get() = fluidRL("molten_iron", true);
+
+	val LUBRICANT: FluidEntry<BaseFlowingFluid.Flowing?>? = REGISTRATE.fluid("lubricant",LUBRICANT_STILL,LUBRICANT_FLOWING)
 		.properties { b -> b.viscosity(1500)
 				.density(500)
 		}
@@ -24,10 +39,16 @@ object CreateOreDepositsFluids {
 				.explosionResistance(100f)
 		}.register()
 
-
 	val SLAG: FluidEntry<BaseFlowingFluid.Flowing?>? =
-		REGISTRATE.fluid("slag", CreateOreDeposits.rl("block/fluid/slag_still"), CreateOreDeposits.rl("block/fluid/slag_flow"))
-			.properties{b -> b.viscosity(1500).density(500)}.fluidProperties{p -> p.levelDecreasePerBlock(2).tickRate(25).slopeFindDistance(3).explosionResistance(0f)}
-			.block{p, t -> SlagFluidBlock(p, t)}.build()
+		REGISTRATE.fluid("slag", SLAG_STILL, SLAG_FLOWING)
+			.properties{ _ -> BlockBehaviour.Properties.ofFullCopy(Blocks.LAVA)}.fluidProperties{p -> p.levelDecreasePerBlock(2).tickRate(25).slopeFindDistance(3).explosionResistance(0f)}
+			.block{f, p -> SlagFluidBlock(f, p)}.build()
+			.register()
+
+	val MOLTEN_IRON: FluidEntry<BaseFlowingFluid.Flowing?>? =
+		REGISTRATE.fluid("molten_iron", MOLTEN_IRON_STILL, MOLTEN_IRON_FLOWING)
+			.properties { _ -> BlockBehaviour.Properties.ofFullCopy(Blocks.LAVA)}
+			.fluidProperties{p -> p.levelDecreasePerBlock(2).tickRate(25).slopeFindDistance(3).explosionResistance(0f)}
+			.block{f,p -> MoltenIronFluidBlock(f,p)}.build()
 			.register()
 }
---
+++ src/main/java/com/createcivilization/create_ore_deposits/registry/fluid/entries/MoltenIronFluidBlock.kt
@@ -0,0 +1,20 @@
+package com.createcivilization.create_ore_deposits.registry.fluid.entries
+
+import net.minecraft.core.BlockPos
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.LiquidBlock
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.material.FlowingFluid
+
+class MoltenIronFluidBlock(fluid: FlowingFluid, properties: Properties) : LiquidBlock(fluid, properties) {
+
+	override fun entityInside(state: BlockState, level: Level, pos: BlockPos, entity: Entity) {
+		if (entity is LivingEntity) {
+			entity.hurt(level.damageSources().inFire(), 4.0f);
+			entity.igniteForSeconds(3f);
+		}
+		super.entityInside(state, level, pos, entity)
+	}
+}
