a6c7f4173744bb33a74269b28eb6265d587791aa
McArctic <creepergaming490@gmail.com>
2025-10-27 13:35:53 -0400

    Added Slag. No Textures. No Problem.

--- src/main/java/com/createcivilization/create_ore_deposits/registry/fluid/CreateOreDepositsFluids.kt
+++ src/main/java/com/createcivilization/create_ore_deposits/registry/fluid/CreateOreDepositsFluids.kt
@@ -13,11 +13,7 @@ import net.neoforged.neoforge.fluids.BaseFlowingFluid
 object CreateOreDepositsFluids {
 
 
-	val LUBRICANT: FluidEntry<BaseFlowingFluid.Flowing?>? = REGISTRATE.fluid(
-		"lubricant",
-		CreateOreDeposits.rl("block/fluid/lubricant_still"),
-		CreateOreDeposits.rl("block/fluid/lubricant_flow")
-	)
+	val LUBRICANT: FluidEntry<BaseFlowingFluid.Flowing?>? = REGISTRATE.fluid("lubricant",CreateOreDeposits.rl("block/fluid/lubricant_still"),CreateOreDeposits.rl("block/fluid/lubricant_flow"))
 		.properties { b -> b.viscosity(1500)
 				.density(500)
 		}
@@ -26,4 +22,10 @@ object CreateOreDepositsFluids {
 				.slopeFindDistance(3)
 				.explosionResistance(100f)
 		}.register()
+
+
+	val SLAG: FluidEntry<BaseFlowingFluid.Flowing?>? = REGISTRATE.fluid("slag", CreateOreDeposits.rl("block/fluid/slag_still"), CreateOreDeposits.rl("block/fluid/slag_flow"))
+		.properties {b -> b.viscosity(1500).density(500)}.
+		fluidProperties {p -> p.levelDecreasePerBlock(2).tickRate(25).slopeFindDistance(25).explosionResistance(0f)}
+	.register()
 }
