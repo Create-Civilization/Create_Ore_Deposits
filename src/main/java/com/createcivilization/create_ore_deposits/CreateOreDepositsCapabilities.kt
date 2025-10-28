ff37412c9b6080cd89923f1f672a63feed9bae43
Orion <90088721+banana-boye@users.noreply.github.com>
2025-10-28 23:22:18 +0000

    Added Capability register and Deposit drill item handling

---
+++ src/main/java/com/createcivilization/create_ore_deposits/CreateOreDepositsCapabilities.kt
@@ -0,0 +1,21 @@
+package com.createcivilization.create_ore_deposits
+
+import com.createcivilization.create_ore_deposits.registry.block.CreateOreDepositsBlockEntities
+import com.createcivilization.create_ore_deposits.registry.block.entries.deposit_drill.DepositDrillBlockEntity
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.neoforge.capabilities.Capabilities
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent
+
+@Suppress("unused")
+@EventBusSubscriber(modid = CreateOreDeposits.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+object CreateOreDepositsCapabilities {
+	@SubscribeEvent
+	fun onRegisterCapabilities(event: RegisterCapabilitiesEvent) {
+		event.registerBlockEntity(
+			Capabilities.ItemHandler.BLOCK,
+			CreateOreDepositsBlockEntities.DEPOSIT_DRILL,
+			DepositDrillBlockEntity::getItemHandler
+		)
+	}
+}
--- src/main/java/com/createcivilization/create_ore_deposits/registry/block/entries/deposit_drill/DepositDrillBlockEntity.kt
+++ src/main/java/com/createcivilization/create_ore_deposits/registry/block/entries/deposit_drill/DepositDrillBlockEntity.kt
@@ -1,12 +1,20 @@
 package com.createcivilization.create_ore_deposits.registry.block.entries.deposit_drill
 
 import com.simibubi.create.content.kinetics.base.BlockBreakingKineticBlockEntity
+import com.simibubi.create.foundation.utility.BlockHelper
 import com.simibubi.create.foundation.utility.ServerSpeedProvider
 import net.createmod.catnip.animation.LerpedFloat
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.neoforged.neoforge.items.IItemHandler
+import net.neoforged.neoforge.items.ItemStackHandler
+import java.util.function.Consumer
 import kotlin.math.roundToInt
 
 // Minimum value for LerpedFloat to not get jumpy
@@ -21,22 +29,48 @@ class DepositDrillBlockEntity(
 	// The Float value distance from the bottom of the drill, should always be positive
 	private var drillOffset: Float = 0f
 	private var lerpedOffset: LerpedFloat = LerpedFloat.linear().startWithValue(min)
+	private lateinit var lastBlock: Block
+
+	private val itemHandler: IItemHandler = ItemStackHandler()
 
 	override fun tick() {
 		super.tick()
+
+		// Compiler inlining will optimize this don't worry.
+		val targetBlock = getTargetBlock()
+		val targetBlockIsAir = targetBlock == Blocks.AIR
+		val itemStack = itemHandler.getStackInSlot(0)
+		val itemStackIsFull = itemStack.count != itemHandler.getSlotLimit(0)
+		val targetBlockIsTheSameAsLastBlock = targetBlock == lastBlock
 		val movementSpeed = getMovementSpeed()
-		if (level?.getBlockState(getTargetPos())?.block == Blocks.AIR || movementSpeed < 0) {
+
+		if ((targetBlockIsAir && (itemStack.isEmpty || (itemStackIsFull && targetBlockIsTheSameAsLastBlock))) || movementSpeed < 0) {
 			drillOffset = (movementSpeed + drillOffset).coerceAtLeast(0f)
+			lerpedOffset.forceNextSync()
 			setLerpedOffset(drillOffset)
-		} else setLerpedOffset(drillOffset.roundToInt())
+		} else {
+			lerpedOffset.forceNextSync()
+			setLerpedOffset(drillOffset.roundToInt())
+		}
+	}
+
+	override fun onBlockBroken(stateToBreak: BlockState) {
+		lastBlock = getTargetBlock()
+		BlockHelper.destroyBlock(level, breakingPos, 1f, Consumer { drops: ItemStack ->
+			itemHandler.insertItem(0, drops, false)
+		})
+	}
+
+	override fun getBreakingPos(): BlockPos {
+		return getTargetPos()
 	}
 
 	fun setLerpedOffset(value: Number) {
 		lerpedOffset.setValue(value.toDouble().coerceAtLeast(min))
 	}
 
-	override fun getBreakingPos(): BlockPos {
-		return getTargetPos()
+	fun getTargetBlock() : Block {
+		return level?.getBlockState(getTargetPos())?.block!!
 	}
 
 	fun getTargetPos() : BlockPos {
@@ -52,4 +86,13 @@ class DepositDrillBlockEntity(
 		if (level!!.isClientSide) movementSpeed *= ServerSpeedProvider.get()
 		return movementSpeed
 	}
+
+	private val facingAxis: Direction.Axis = blockState.getValue(BlockStateProperties.HORIZONTAL_AXIS)
+
+	fun getItemHandler(direction: Direction): IItemHandler? {
+		// Checks if the axis is the same as the facing axis and if the axis direction is the opposite of the facing
+		// In short, checks if the direction is the opposite of the facing direction
+		return if (direction.axis == facingAxis && direction.axisDirection == Direction.AxisDirection.NEGATIVE) itemHandler else null
+	}
+
 }
