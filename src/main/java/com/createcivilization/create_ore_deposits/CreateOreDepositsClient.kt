6ea98f22e63cc14c0f0a7600e3273aac1373a7ae
EpicVon2468 <thomaschad0909@gmail.com>
2025-10-30 15:33:09 +1100

    Inline client stuff

--- src/main/java/com/createcivilization/create_ore_deposits/CreateOreDepositsClient.kt
+++ src/main/java/com/createcivilization/create_ore_deposits/CreateOreDepositsClient.kt
@@ -1,11 +1,16 @@
 package com.createcivilization.create_ore_deposits
 
 import com.createcivilization.create_ore_deposits.registry.block.entries.deposit_drill.DepositDrillBlockModels
+import com.createcivilization.create_ore_deposits.registry.block.entries.deposit_drill.DrillBlockModel
 import com.createcivilization.create_ore_deposits.util.logI
+import com.createcivilization.create_ore_deposits.util.resource
+
+import net.minecraft.client.model.geom.ModelLayerLocation
 
 import net.neoforged.api.distmarker.Dist
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
+import net.neoforged.neoforge.client.event.EntityRenderersEvent.RegisterLayerDefinitions
 
 import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 
@@ -14,6 +19,7 @@ object CreateOreDepositsClient {
 
 	init {
 		MOD_BUS.addListener(::clientInit)
+		MOD_BUS.addListener(::onRegisterLayerDefinitions)
 	}
 
 	@Suppress("UnusedExpression") // Calls static initialiser
@@ -22,4 +28,10 @@ object CreateOreDepositsClient {
 		DepositDrillBlockModels
 //		PonderIndex.addPlugin(CODPonderPlugin())
 	}
+
+	fun onRegisterLayerDefinitions(event: RegisterLayerDefinitions) {
+		event.registerLayerDefinition(DRILL_LAYER, DrillBlockModel::createModel)
+	}
+
+	val DRILL_LAYER: ModelLayerLocation = ModelLayerLocation("drill".resource(), "main")
 }
