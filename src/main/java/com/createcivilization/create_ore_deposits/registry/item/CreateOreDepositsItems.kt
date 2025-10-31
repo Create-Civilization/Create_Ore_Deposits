819ae350ca6c58a9b4a6d8da856e3c31779888d9
McArctic <creepergaming490@gmail.com>
2025-10-30 23:32:24 -0400

    Deleted Items, As they are all changed on refinement branch

--- src/main/java/com/createcivilization/create_ore_deposits/registry/item/CreateOreDepositsItems.kt
+++ src/main/java/com/createcivilization/create_ore_deposits/registry/item/CreateOreDepositsItems.kt
@@ -1,9 +1,11 @@
 package com.createcivilization.create_ore_deposits.registry.item
 
 import com.createcivilization.create_ore_deposits.CreateOreDeposits
+import com.createcivilization.create_ore_deposits.CreateOreDeposits.REGISTRATE
 import com.createcivilization.create_ore_deposits.util.Item
 import com.createcivilization.create_ore_deposits.util.ItemProvider
 import com.createcivilization.create_ore_deposits.util.KotlinDeferredRegister
+import com.tterrag.registrate.util.entry.ItemEntry
 
 import net.minecraft.core.registries.Registries
 import net.minecraft.world.item.Item
@@ -12,21 +14,5 @@ import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 
 object CreateOreDepositsItems {
 
-	@JvmField
-	internal val ITEM_PROVIDER: KotlinDeferredRegister<Item> = KotlinDeferredRegister(
-		Registries.ITEM,
-		CreateOreDeposits.MOD_ID
-	)
 
-	// Iron Stuff
-	private val _UNREFINED_IRON_ORE_ITEM: ItemProvider = ITEM_PROVIDER.register("unrefined_iron_ore", ::Item)
-	val UNREFINED_IRON_ORE_ITEM: Item get() = _UNREFINED_IRON_ORE_ITEM()
-
-	private val _UNREFINED_IRON_ORE_POWDER_ITEM: ItemProvider = ITEM_PROVIDER.register("unrefined_iron_ore_powder", ::Item)
-	val UNREFINED_IRON_ORE_POWDER_ITEM: Item get() = _UNREFINED_IRON_ORE_POWDER_ITEM()
-
-	private val _IRON_ORE_POWDER_ITEM: ItemProvider = ITEM_PROVIDER.register("iron_ore_powder", ::Item)
-	val IRON_ORE_POWDER_ITEM: Item get() = _IRON_ORE_POWDER_ITEM()
-
-	init { ITEM_PROVIDER.register(MOD_BUS) }
 }
