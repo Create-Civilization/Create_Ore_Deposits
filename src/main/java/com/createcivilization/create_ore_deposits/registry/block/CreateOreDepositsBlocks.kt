f45cc0f095d2fe2975f2608d8f7ebfa0aa7e6e55
mcarctic <CreeperGaming490@gmail.com>
2025-11-18 13:17:48 -0500

    Remove Dead Imports

--- src/main/java/com/createcivilization/create_ore_deposits/registry/block/CreateOreDepositsBlocks.kt
+++ src/main/java/com/createcivilization/create_ore_deposits/registry/block/CreateOreDepositsBlocks.kt
@@ -1,32 +1,13 @@
 package com.createcivilization.create_ore_deposits.registry.block
 
-import com.createcivilization.create_ore_deposits.CreateOreDeposits
 import com.createcivilization.create_ore_deposits.CreateOreDeposits.REGISTRATE
 import com.createcivilization.create_ore_deposits.registry.block.entries.deposit_drill.DepositDrillBlock
-import com.createcivilization.create_ore_deposits.registry.tab.CreateOreDepositsTabs
-import com.createcivilization.create_ore_deposits.util.BlockProvider
-import com.createcivilization.create_ore_deposits.util.ItemProvider
 import com.simibubi.create.AllBlocks
-import com.simibubi.create.AllTags.AllBlockTags
-import com.simibubi.create.foundation.data.AssetLookup
-import com.simibubi.create.foundation.data.CreateRegistrate
 import com.simibubi.create.foundation.data.SharedProperties
-import com.simibubi.create.foundation.data.TagGen
-import com.tterrag.registrate.builders.BlockBuilder
-import com.tterrag.registrate.providers.DataGenContext
-import com.tterrag.registrate.providers.RegistrateBlockstateProvider
 import com.tterrag.registrate.util.entry.BlockEntry
-import com.tterrag.registrate.util.nullness.NonNullBiConsumer
-import com.tterrag.registrate.util.nullness.NonNullFunction
-import com.tterrag.registrate.util.nullness.NonNullSupplier
-import com.tterrag.registrate.util.nullness.NonNullUnaryOperator
-import net.minecraft.client.renderer.RenderType
-import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockBehaviour
-import net.minecraft.world.level.material.MapColor
-import java.util.function.Supplier
 
 object CreateOreDepositsBlocks {
 
