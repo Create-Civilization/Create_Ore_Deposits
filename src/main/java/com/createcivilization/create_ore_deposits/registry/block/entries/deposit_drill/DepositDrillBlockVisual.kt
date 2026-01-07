f3464edb6e9ac9df4e809ec0ae8294900ad934db
McArctic <creepergaming490@gmail.com>
2026-01-07 05:17:17 -0500

    This is def on the wrong branch but whatever.

--- src/main/java/com/createcivilization/create_ore_deposits/registry/block/entries/deposit_drill/DepositDrillBlockVisual.kt
+++ src/main/java/com/createcivilization/create_ore_deposits/registry/block/entries/deposit_drill/DepositDrillBlockVisual.kt
@@ -12,12 +12,18 @@ import com.simibubi.create.content.processing.burner.ScrollInstance
 import com.simibubi.create.foundation.render.AllInstanceTypes
 
 import dev.engine_room.flywheel.api.instance.Instancer
+import dev.engine_room.flywheel.api.model.Model
 import dev.engine_room.flywheel.api.visualization.VisualizationContext
 import dev.engine_room.flywheel.lib.instance.InstanceTypes
 import dev.engine_room.flywheel.lib.instance.TransformedInstance
 import dev.engine_room.flywheel.lib.model.Models
+import dev.engine_room.flywheel.lib.model.baked.BakedModelBuilder
 
 import net.createmod.catnip.render.SpriteShiftEntry
+import net.minecraft.client.Minecraft
+import net.minecraft.client.resources.model.BakedModel
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
 
 class DepositDrillBlockVisual(
 	dispatcher: VisualizationContext,
@@ -28,8 +34,20 @@ class DepositDrillBlockVisual(
 	override fun getRopeModel(): Instancer<TransformedInstance> =
 		instancerProvider().instancer(InstanceTypes.TRANSFORMED, Models.partial(HOSE))
 
-	override fun getMagnetModel(): Instancer<TransformedInstance> =
-		instancerProvider().instancer(InstanceTypes.TRANSFORMED, Models.partial(DRILL_MAGNET))
+
+//	override fun getMagnetModel(): Instancer<TransformedInstance> =
+//		instancerProvider().instancer(InstanceTypes.TRANSFORMED, Models.partial(DRILL_MAGNET))
+
+	override fun getMagnetModel(): Instancer<TransformedInstance> {
+		var stack = blockEntity.getDrillTipItemHandler().getStackInSlot(0)
+		if(stack.isEmpty){
+			return instancerProvider().instancer(InstanceTypes.TRANSFORMED, Models.partial(DRILL_MAGNET))
+		}
+		val minecraft = Minecraft.getInstance()
+		val bakedModel: BakedModel = minecraft.itemRenderer.getModel(stack, null, null, 0)
+		val model : Model = BakedModelBuilder(bakedModel).build()
+		return instancerProvider().instancer(InstanceTypes.TRANSFORMED, model)
+	}
 
 	override fun getHalfMagnetModel(): Instancer<TransformedInstance> =
 		instancerProvider().instancer(InstanceTypes.TRANSFORMED, Models.partial(HOSE_HALF_MAGNET))
