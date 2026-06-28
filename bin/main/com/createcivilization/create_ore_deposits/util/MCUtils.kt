b06938e4cb32e7fea7de21f06e507cfca5717d16
starpanda24825 <starpanda24825@gmail.com>
2026-06-28 02:10:59 +0100

    Stop tracking bin/ and .vscode/ build/editor artifacts

--- bin/main/com/createcivilization/create_ore_deposits/util/MCUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.createcivilization.create_ore_deposits.util
-
-import com.createcivilization.create_ore_deposits.CreateOreDeposits
-
-import net.minecraft.resources.ResourceLocation
-
-fun String.asResource(): ResourceLocation = ResourceLocation.fromNamespaceAndPath(CreateOreDeposits.MOD_ID, this)
